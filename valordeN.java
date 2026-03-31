1863947b1d5ad10d66ada04204d290432babd635
yasmin2006-create <yasminlouisefontes1507@gmail.com>
2026-03-30 22:49:51 -0300

    Remove redundant lines and clean up formatting

--- valordeN.java
+++ valordeN.java
@@ -1,24 +1,19 @@
 // Yasmin louise Fontes Guimarães
 // 12661935138
 import java.util.Scanner;
-
 public class valordeN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Digite N: ");
         int N = sc.nextInt();
-
         if (N <= 0) {
-            System.out.println("valor inválido. N deve ser maior que zero.");
+        System.out.println("valor inválido. N deve ser maior que zero.");
         } else {
-            int contador = 1; // inicializa a variável
-
-            while (contador <= N) {
-                System.out.println(contador);
+         int contador = 1; // inicializa a variável
+         while (contador <= N) {
+          System.out.println(contador);
                 contador++; // incrementa
             }
-
             System.out.println("Contagem concluída.");
         }
 
